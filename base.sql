db8dbc4a1811ed903fd7a465d2aaae25ed286f7a
Océane Bondon <oceanebondon30@gmail.com>
2024-05-02 11:00:34 +0200

    commit 2

--- base.sql
+++ base.sql
@@ -1,18 +1,18 @@
-DROP DATABASE IF EXISTS bdd;
-CREATE DATABASE bdd CHARACTER SET = 'utf8' COLLATE ='utf8_general_ci';
-USE bdd;
+DROP DATABASE IF EXISTS blog;
+CREATE DATABASE blog CHARACTER SET = 'utf8' COLLATE ='utf8_general_ci';
+USE blog;
 
 CREATE TABLE t_billet(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    date,
-    titre VARCHAR(50),
-    contenu VARCHAR(50)
+    date datetime,
+    titre VARCHAR(100),
+    contenu VARCHAR(400)
 );
 
 CREATE TABLE t_commentaire(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    date,
-    auteur VARCHAR(50),
-    contenu VARCHAR(50),
+    date datetime,
+    auteur VARCHAR(100),
+    contenu VARCHAR(200),
     id_billet INT NOT NULL
 );
